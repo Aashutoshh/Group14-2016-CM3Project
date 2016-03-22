d75d0b90624d6b42a1639d0f8c2f23fb10ba8a50
randalx2 <pmisthry@gmail.com>
2016-03-22 23:11:59 +0200

    Updating the Base class header and cpp files in Master. Added comments

--- Galaxy-Guardian/BaseObject.cpp
+++ Galaxy-Guardian/BaseObject.cpp
@@ -6,16 +6,22 @@
 /*Class Definitions for Methods, Constructors and Destructors Here!!*/
 BaseObject::BaseObject()
 {
-	x = 0;
+	//Default Base class constructor. Due to inheritance each sub class object also attains their own instances
+	//of these attributes wheneve they are created
+
+	x = 0;  //Object position
 	y = 0;
-	speedX = 0;
+
+	speedX = 0;  //Object speed
 	speedY = 0;
-	directX = 0;
+
+	directX = 0; //Object direction of movement
 	directY = 0;
-	boundaryX = 0;
+
+	boundaryX = 0; //boundaries of surrounding object's area
 	boundaryY = 0;
 
-	 maxFrame = 0;
+	 maxFrame = 0;  //Necessary animation allegro attributes for animating an object
 	 curFrame= 0;
 	 frameCount=0;
 	 frameDelay=0;
@@ -23,20 +29,28 @@ BaseObject::BaseObject()
 	 frameHeight=0;
 	 animationColumns=0;
 	 animationDirection=0;
+
 	 image = NULL;
 	onScreen = true;
 	checkCollision = true;
-
 
 }
 
 
 BaseObject::~BaseObject()
 {
+	//Default Base Class blank Destructor
 }
+
+void BaseObject::DestroyObject()
+{
+	//Similar to a blank destructor except this can be overridden by sub class objects
+	//to release their own associated resources. Demos inheritance and polymorphism
+}
+
 void BaseObject::init(float ix, float iy, float ispeedX, float ispeedY, int idirectX, int idirectY, int iboundaryX, int iboundaryY){
 
-	// performs generic setup of the common variables used in the program
+	// performs generic setup of the common variables used in the program by all objects
 	x = ix;
 	y = iy;
 
@@ -49,9 +63,59 @@ void BaseObject::init(float ix, float iy, float ispeedX, float ispeedY, int idir
 	boundaryX = iboundaryX;
 	boundaryY = iboundaryY;
 
+}
 
+//Methods for updating object position and rendering
+
+void BaseObject::UpdateObject(){
+
+	//Every object will update in a similar manner depending on its x,y position and
+	//speedx, speedy values.
+	//If an object is meant to be stationery we just set its speed to 0 stopping its updation any further
+
+	x += speedX*directX;
+	y += speedY*directY;
+}
+
+void BaseObject::RenderObject(){
+	//This only exists in base class to be made virtual so sub classes can override it with their own rendering
+}
+
+bool BaseObject::getCollision(BaseObject *otherObject)
+{
+	//Get the x,y position of the incoming input object in temp variables
+	float otherX = otherObject->getX();
+	float otherY = otherObject->getY();
+
+	//Get the boundaries surrounding the incoming object in temp variables
+	float oboundX = otherObject->getX();
+	float oboundY = otherObject->getY();
+
+	//Check for collision conditions between the 2 objects
+	//Collision checking algortihm based on object centers and boundaries
+	if (x + oboundX > otherX - oboundX &&
+		x - oboundX < otherX + oboundX &&
+		y + oboundY > otherY - oboundY &&
+		y - oboundY < otherY + oboundY)
+
+	{
+		return true;
+	}
+	else
+		return false;
 }
 
 void BaseObject::collided(int iobjType){
-	// exists to be virtual to give sub classes the ability to override it.
+	//Only exists to be virtual to give sub classes the ability to override it based on their objects collision status
 }
+
+bool BaseObject::onScreenCollide()
+{
+	//Will only return true if an object is BOTH alive and collidable
+	//Otherwise it returns a false
+	return onScreen && checkCollision;
+}
+	
+
+
+
--- Galaxy-Guardian/BaseObject.h
+++ Galaxy-Guardian/BaseObject.h
@@ -14,25 +14,26 @@ using namespace std;
 
 class BaseObject{
 private: 
-	int objType;
-	bool onScreen;
+	int objType;        //Integer value assigned to I.D the player, enemy or any other screen object
+	bool onScreen;       //bool attribute to check if an object is still onscreen
 	bool checkCollision; // collision status
 
 protected :
-	float x, y; // position
+	float x, y;            // position
 	float speedX, speedY; // speed of object
 
 	int directX, directY; //direction of movement 
 
-	int boundaryX, boundaryY; //boundary condition
+	int boundaryX, boundaryY; //boundary of area surrounding an object
 
 //Attributes for animation and rendering 
-	int maxFrame;
-	int curFrame;
-	int frameCount;
-	int frameDelay;
-	int frameWidth;
-	int frameHeight;
+
+	int maxFrame;   //Maximum amount of frames in a row of a spritesheet
+	int curFrame;   //Current frame in a row of a sprite sheet
+	int frameCount; //frame count to help set FPS in game
+	int frameDelay; 
+	int frameWidth; //Width of sprite image from an index in the sprite sheet
+	int frameHeight; //Height of sprite image from an index in the sprite sheet
 	int animationColumns;
 	int animationDirection;
 
@@ -42,9 +43,18 @@ protected :
 public:
 	BaseObject();
 	~BaseObject();
+
+	void virtual DestroyObject(); //Meant to do what a destructor does but instead this is a virtual function that
+	                              //uses inheritance i.e it can be overriden when subclasses make the same call to release
+	                              //resources allocated to that particular object of any sub class. It some cases this is
+	                              //the better option instead of using a default destructor from the base class
+
 	void init(float,float,float,float,int,int,int,int); // initialization of any object method
 
 	// methods for animation and rendering
+	void virtual UpdateObject();  //Virtual functions using inheritance--Gives sub classes opportunity to use them and
+	void virtual RenderObject();  //override them accordingly
+
 
 	//Accessor and Mutator with definition
 	float getX(){ return x;  }
@@ -57,7 +67,7 @@ public:
 	void setX(int ix){ x = ix; }
 	void setY(int iy){ y = iy; }
 
-	void setObjType(int iobjType){ objType = iobjType; }
+	void setObjType(int iobjType){ objType = iobjType; } 
 	int getObjType(){ return objType; }
 
 	bool getOnScreen(){ return onScreen; }
@@ -66,13 +76,12 @@ public:
 	bool getCheckCollision(){ return checkCollision; }
 	void setCheckCollision(bool icheckCollision){ checkCollision = icheckCollision; }
 
-	bool getCollision(){ BaseObject *otherObject; }// checks if current object collided with input object
-	void virtual collided(int iobjType); // is called when collision takes place 
+	bool getCollision(BaseObject *otherObject);// checks if current object collided with input object
 
-
-
-
-
+	void virtual collided(int iobjType); //Check if current object collided with another
+	
+	bool onScreenCollide(); //Combines the onScreen() and checkCollission() methods to only output a true value
+	                       //only if an object is both onscreen and is collidable 
 
 };
 
