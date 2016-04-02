9953f09d83676a3a9940ce76ba202ceb773e7395
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-04-02 17:29:58 +0200

    Commit 2

--- Galaxy-Guardian/Bulllet.cpp
+++ Galaxy-Guardian/Bulllet.cpp
@@ -3,11 +3,24 @@
 
 
 // Bullet Class methods 
+
+//  Default constructor
 Bulllet::Bulllet()
 {
 }
 
-
+//Default destructor
 Bulllet::~Bulllet()
 {
 }
+
+
+Bulllet::Bulllet(float x, float y, void(*ScorePoint)(void))
+{
+	BaseObject::Init(x, y, 10, 0, 1, 0, 0, 0);
+
+	setObjType(BULLET);
+
+	Bulllet::ScorePoint = ScorePoint;
+}
+
--- Galaxy-Guardian/Bulllet.h
+++ Galaxy-Guardian/Bulllet.h
@@ -1,12 +1,20 @@
 #pragma once
 #include "BaseObject.h"
+#include <string>
+using namespace std;
+
 
 //Declaration for spaceship bullet class
 
-class Bulllet
-{
+class Bulllet: public BaseObject{
+
+	// private variables 
+private: 
+	void(*ScorePoint)(void);
+	// public variables 
 public:
 	Bulllet();
 	~Bulllet();
+	Bulllet(float x, float y, void(*ScorePoint)(void));
 };
 
