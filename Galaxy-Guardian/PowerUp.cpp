5f988bd9f9208a86d0ca38c9270fe024f526ad75
Aashutoshh <214506607@stu.ukzn.ac.za>
2016-04-05 00:33:37 +0200

    Addded headers for power up

--- Galaxy-Guardian/PowerUp.cpp
+++ Galaxy-Guardian/PowerUp.cpp
@@ -3,6 +3,7 @@
 
 PowerUp::PowerUp()
 {
+	
 }
 
 
--- Galaxy-Guardian/PowerUp.h
+++ Galaxy-Guardian/PowerUp.h
@@ -7,6 +7,18 @@ class PowerUp
 {
 public:
 	PowerUp();
+	PowerUp(float X, float Y, ALLEGRO_BITMAP *iimage); //Overloading constructor
 	~PowerUp();
+
+	
+	//functions inherateded from base class
+
+	void DestroyObject(); //destroy object of type power up
+
+	void UpdateObject();  //update object power up
+	
+	void RenderObject(); // render object of power up
+	
+	void Collided(int objectID);  //collision
 };
 
