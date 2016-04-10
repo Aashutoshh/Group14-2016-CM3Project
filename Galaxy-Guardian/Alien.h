43a7395ceaaab67e854b7a358d16d2e15fa9d50f
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-04-10 10:46:06 +0200

    Define attributes and functions of Alien Class

--- Galaxy-Guardian/Alien.h
+++ Galaxy-Guardian/Alien.h
@@ -8,8 +8,20 @@ using namespace std;
 
 class Alien : public BaseObject  //Inheriting attributes of base class
 {
+private: int alienHP;                        //Alien health points--> is increased with difficulty
+		 //Using Dynamic Memory Allocation for 2D ARRAY Using pointer method
+		 Alien** alienGrid = new Alien*[6]; //Grid of Aliens 6 X 6
+		 void(*TakeLife)(void);
+		 float tempX, tempY;
+
 public:
 	Alien();
 	~Alien();
+	void InitAlien(float x, float y, ALLEGRO_BITMAP *iimage, void(*TakeLife)(void));
+
+	void DestroyObject(); //virtual function
+	void UpdateObject(); //virtual function
+	void RenderObject(); //virtual function
+	void Collided(int objectType); //virtual function
 };
 
