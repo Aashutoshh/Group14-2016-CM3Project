0ca6bfde18094a544d0ab95b6bfba8d1abfcfc36
randalx2 <pmisthry@gmail.com>
2016-04-11 22:10:20 +0200

    Updated Globals.h and Spaceship.cpp

--- Galaxy-Guardian/Global.h
+++ Galaxy-Guardian/Global.h
@@ -3,8 +3,7 @@
 //GLOBAL VARIABLES FOR THE ENTIRE PROJECT
 
 const int width = 800;
-const int height = 400;
-enum ID{ PLAYER, ENEMY, BULLET, BORDER, MISC, EXPLOSION, POWERUP }; //ID array for player and other objects
-enum STATE{ TITLE, PLAYING, LOST }; //3 stage finite state machine array. To be used later in development
-
-//ADDED IN POWER UP
+const int height = 600;
+enum ID{ PLAYER, ENEMY, BULLET, BORDER, MISC, EXPLOSION, POWER, ALIEN, ALIENBULLET, BOSS }; //ID array for player and other objects
+enum STATE{ TITLE, PLAYING, LOST, LEVEL }; //3 stage finite state machine array. To be used later in development
+//To add levelling add to this state machine
