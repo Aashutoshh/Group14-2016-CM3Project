75e7acbfad5cd1c31060886521de30513f5bb90c
randalx2 <pmisthry@gmail.com>
2016-04-22 21:49:07 +0200

    Created skeleton code in alien.cpp to resolve externals

--- Galaxy-Guardian/Alien.cpp
+++ Galaxy-Guardian/Alien.cpp
@@ -1,5 +1,10 @@
 #include "Alien.h"
 #include "BaseObject.h"
+#include <string>
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
 
 Alien::Alien()
 {
@@ -8,4 +13,32 @@ Alien::Alien()
 
 Alien::~Alien()
 {
+}
+
+void Alien::InitAlien(float x, float y, ALLEGRO_BITMAP *iimage, void(*TakeLife)(void))
+{
+
+}
+
+void Alien::DestroyObject()
+{
+	BaseObject::DestroyObject();
+
+}
+
+void Alien::UpdateObject()
+{
+	BaseObject::UpdateObject();
+
+}
+
+void Alien::RenderObject()
+{
+	BaseObject::RenderObject();
+
+}
+
+void Alien::Collided(int objectType)
+{
+
 }
