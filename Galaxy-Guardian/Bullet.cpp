e470eacb2f6c8e1817773ea90e475461f47c9da3
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-04-23 15:05:34 +0200

    Define bullet constructor and setup methods to be used in bullet class

--- Galaxy-Guardian/Bullet.cpp
+++ Galaxy-Guardian/Bullet.cpp
@@ -4,11 +4,44 @@
 
 using namespace std;
 
-Bullet::Bullet()
+Bullet::Bullet(float x, float y, void(*enemiesDown)(void), void(*bossesKilled)(void), ALLEGRO_BITMAP *iimage)
 {
+	BaseObject::Init(x, y, 10, 0, 1, 0, 0, 0);
+
+	setObjType(BULLET);
+	image = iimage;
+
+	//Bullet Animation
+	maxFrame = 5;  //Necessary animation allegro attributes for animating an object
+	curFrame = 0;
+	frameCount = 0;
+	frameDelay = 8;
+	frameWidth = 32;
+	frameHeight = 21;
+
+	Bullet::enemiesDown = enemiesDown;
+	Bullet::bossesKilled = bossesKilled;
 }
 
 
-Bullet::~Bullet()
-{
+void Bullet::DestroyObject(){
+	
+}
+
+void Bullet::UpdateObject(){
+	
+}
+
+void Bullet::RenderObject(){
+
+	
+
+	
+}
+
+void Bullet::Collided(int objectID){
+	
+}
+
+Bullet::~Bullet(){
 }
