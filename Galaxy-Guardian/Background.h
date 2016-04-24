f3ef7af8934c51aa18e2a3b57e29586e7b0c4022
Sanath Maharaj <sanathm7@gmail.com>
2016-04-24 11:20:59 +0200

    add Background class

--- Galaxy-Guardian/Background.h
+++ Galaxy-Guardian/Background.h
@@ -1,12 +1,19 @@
 #pragma once
+
+#include <string>
 #include "BaseObject.h"
 
 //Declaration for stage background class
 
-class Background
+class Background:public BaseObject
 {
 public:
-	Background();
+	Background(ALLEGRO_BITMAP *iimage, float ispeedX);
 	~Background();
+
+	//Overriding functions for base class virtual functions
+	void DestroyObject();
+	void UpdateObject();
+	void RenderObject();
 };
 
