5bf6f19f0a8d88b89338460e0ee50eb358d28ae6
Aashutoshh <214506607@stu.ukzn.ac.za>
2016-04-27 18:45:27 +0200

    Explosion constructor

--- Galaxy-Guardian/Explosion.cpp
+++ Galaxy-Guardian/Explosion.cpp
@@ -1,11 +1,32 @@
 #include "Explosion.h"
 #include "BaseObject.h"
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cmath>
+#include <ctime>
 
-Explosion::Explosion()
+using namespace std;
+
+Explosion::Explosion(float ix, float iy, ALLEGRO_BITMAP *iimage)
 {
+	BaseObject::Init(ix, iy, 0, 0, 0, 0, 0, 0);
+	setObjType(EXPLOSION);
+	setCheckCollision(false);  //not collidable
+
+	maxFrame = 31;        //relative to sprite sheet
+	curFrame = 0;
+	frameCount = 0;
+	frameDelay = 1;
+	frameWidth = 128;
+	frameHeight = 128;
+	animationColumns = 8;
+	animationDirection = 1;
+	image = iimage;
 }
 
 
 Explosion::~Explosion()
 {
+
 }
