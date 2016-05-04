af25d9bbc20510d0278e6440fc6318a2682aad24
Aashutoshh <214506607@stu.ukzn.ac.za>
2016-05-04 22:08:41 +0200

    astroid header file complete

--- Galaxy-Guardian/Asteroid.h
+++ Galaxy-Guardian/Asteroid.h
@@ -1,15 +1,24 @@
 #pragma once
-#include <string>
 #include "BaseObject.h"
+#include <string>
 
 using namespace std;
 
 class Asteroid : public BaseObject
 {
 private:
+	void(*TakeLife)(void);
 
 public:
 	Asteroid();
 	~Asteroid();
+
+	Asteroid(float x, float y, ALLEGRO_BITMAP *iimage, void(*TakeLife)(void));
+
+	//Overriding functions for base class virtual functions
+	void DestroyObject();
+	void UpdateObject();
+	void RenderObject();
+	void Collided(int objectID);
 };
 
